87f74a55295bd15dbcc349ecad21e93ee551e7aa
LukasDSchmid <80950546+LukasDSchmid@users.noreply.github.com>
2021-05-30 14:47:11 +0200

    Abgabe3.1 Fehlerbehebung

--- Abgabe3.1/bspServer.ts
+++ Abgabe3.1/bspServer.ts
@@ -2,25 +2,25 @@ import * as Http from "http";
 
 export namespace P_3_1Server {
     console.log("Starting server");
-    let port: number = Number(process.env.PORT);
+    let port: number = Number(process.env.PORT);        //Variable anlegen, um sich mit dem Server zu verbinden
     if (!port)
-        port = 8100;
+        port = 8100;                                    //Port den Wert 8100 zuweisen
 
-    let server: Http.Server = Http.createServer();
+    let server: Http.Server = Http.createServer();      //Serverr erstellen + Eventlistener
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
 
-    function handleListen(): void {
+    function handleListen(): void {                     // Gibt "Listening" zurück
         console.log("Listening");
     }
 
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {   //Gewährt Zugriff
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
+        _response.write(_request.url);                                                               //Gibt URL zurück
         _response.end();
     }
 }
--- Abgabe3.1/serverScript.ts
+++ Abgabe3.1/serverScript.ts
@@ -1,18 +1,27 @@
 namespace P_3_1Server {
 
-    let formData: FormData = new FormData(document.forms[0]);
+    
 
     let sendButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send");
     sendButton.addEventListener("click", send);
 
     async function send (): Promise<void>{
+        let formData: FormData = new FormData(document.forms[0]);
+        console.log(formData.get("name"));
+        for (let entry of formData) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+
         let url: string = "https://testgislukas.herokuapp.com/";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
         let response: Response = await fetch(url);
         let text: string = await response.text()
         console.log("Answer:");
-        console.log(text);
+        let answerText: HTMLParagraphElement = <HTMLDivElement>document.getElementById("answer");
+        answerText.innerText = text;
     }
         
     
